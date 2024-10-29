73d28fe3eb40b795d7316eb9be72e6c9780c1539
CarolinaOM <caro13081991@gmail.com>
2024-10-29 15:31:17 +0000

    1.1: Información del Curso, paso 2

--- part1Exercise/src/App.jsx
+++ part1Exercise/src/App.jsx
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types';
 
-const Header = ({ course }) => {
+//1.1: Información del Curso, paso 1
+
+/*const Header = ({ course }) => {
   return <h1>{course}</h1>;
 };
 
@@ -27,6 +29,77 @@ const Content = ({ parts }) => {
       {parts.map((part, index) => (
         <Part key={index} name={part.name} exercises={part.exercises} />
       ))}
+    </div>
+  );
+};
+
+Content.propTypes = {
+  parts: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      exercises: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
+
+const Total = ({ parts }) => {
+  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
+  return <p>Number of exercises {totalExercises}</p>;
+};
+
+Total.propTypes = {
+  parts: PropTypes.arrayOf(
+    PropTypes.shape({
+      exercises: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const parts = [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 }
+  ];
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  );
+};*/
+
+//1.2: Información del Curso, paso 2
+const Header = ({ course }) => {
+  return <h1>{course}</h1>;
+};
+
+Header.propTypes = {
+  course: PropTypes.string.isRequired,
+};
+
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+Part.propTypes = {
+  name: PropTypes.string.isRequired,
+  exercises: PropTypes.number.isRequired,
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      <Part name={parts[0].name} exercises={parts[0].exercises} />
+      <Part name={parts[1].name} exercises={parts[1].exercises} />
+      <Part name={parts[2].name} exercises={parts[2].exercises} />
     </div>
   );
 };
