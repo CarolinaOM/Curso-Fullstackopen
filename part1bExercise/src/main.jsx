fab344c846575d0dff98c382880dc71af4e0e481
CarolinaOM <caro13081991@gmail.com>
2024-10-30 16:02:29 +0000

    1.4: Información del Curso paso 4 parte1b

--- part1bExercise/src/main.jsx
+++ part1bExercise/src/main.jsx
@@ -1,4 +1,5 @@
-import PropTypes from 'prop-types';
+//1.3: Información del Curso, paso 3
+/*import PropTypes from 'prop-types';
 
 const Header = (props) => {
   return <h1>{props.course}</h1>;
@@ -89,6 +90,96 @@ const App = () => {
       <Header course={course} />
       <Content part1={part1} part2={part2} part3={part3} />
       <Total part1={part1} part2={part2} part3={part3} />
+    </div>
+  );
+};
+
+export default App;*/
+
+//1.4: Información del Curso paso 4
+import PropTypes from 'prop-types';
+
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+Header.propTypes = {
+  course: PropTypes.string.isRequired,
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  );
+};
+
+Part.propTypes = {
+  part: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    exercises: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
+    </div>
+  );
+};
+
+Content.propTypes = {
+  parts: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      exercises: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises{' '}
+      {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
+    </p>
+  );
+};
+
+Total.propTypes = {
+  parts: PropTypes.arrayOf(
+    PropTypes.shape({
+      exercises: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10,
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7,
+    },
+    {
+      name: 'State of a component',
+      exercises: 14,
+    },
+  ];
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
